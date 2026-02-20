4dc53a972ec1bf4f1463fcd4574081faece345a5
Simon Marchi <simon.marchi@efficios.com>
2026-02-20 13:01:51 -0500

    gdb/dwarf: remove stale comment about abbrev table
    This parameter no longer exist.
    
    Change-Id: I6d296d130bd65b4636c450b1943cbe24959be090

--- gdb/dwarf2/read.c
+++ gdb/dwarf2/read.c
@@ -2913,10 +2913,6 @@ cutu_reader::init_tu_and_read_dwo_dies (dwarf2_per_cu *this_cu,
 /* Initialize a CU (or TU) and read its DIEs.
    If the CU defers to a DWO file, read the DWO file as well.
 
-   ABBREV_TABLE, if non-NULL, is the abbreviation table to use.
-   Otherwise the table specified in the comp unit header is read in and used.
-   This is an optimization for when we already have the abbrev table.
-
    If EXISTING_CU is non-NULL, then use it.  Otherwise, a new CU is
    allocated.  */
 
