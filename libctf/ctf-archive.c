bminor/binutils-gdb

libctf, include, binutils: rationalize CTF dict model, cuname, parent names
The names of these functions are historical and inconsistent with other
functions in the API: they relate to dict-wide properties but are not in
the ctf_dict_* namespace, and some of them take ints that could be
enums.

Fix all of these:

ctf_cuname -> ctf_dict_cuname
ctf_cuname_set -> ctf_dict_set_cuname
ctf_parent_dict -> ctf_dict_parent
ctf_parent_name -> ctf_dict_parent_name
ctf_parent_name_set -> ctf_dict_set_parent_name
ctf_setmodel -> ctf_dict_set_model
ctf_getmodel -> ctf_dict_model
ctf_setspecific -> ctf_dict_specific_set
ctf_getspecific -> ctf_dict_specific

libctf/ctf-archive.c
@@ -212,7 +212,7 @@ ctf_arc_write_fd (int fd, ctf_dict_t **ctf_dicts, size_t ctf_dict_cnt,
      memory.)  */
 
   if (ctf_dict_cnt > 0)
-    archdr->model = ctf_getmodel (ctf_dicts[0]);
+    archdr->model = ctf_dict_model (ctf_dicts[0]);
 
   /* Now write out the CTFs: ctf_archive_modent array via the mapping,
      ctfs via write().  The names themselves have not been written yet: we
@@ -1320,7 +1320,7 @@ ctf_dict_open_by_offset (const struct ctf_archive_internal *arci,
   fp = ctf_bufopen (&ctfsect, symsect, strsect, errp);
   if (fp)
     {
-      ctf_setmodel (fp, arci->ctfi_hdr->model);
+      ctf_dict_set_model (fp, arci->ctfi_hdr->model);
       if (little_endian_symtab >= 0)
 	ctf_symsect_endianness (fp, little_endian_symtab);
     }

RTEMS/sourceware-mirror-binutils-gdb

libctf/ctf-archive.c
@@ -192,7 +192,7 @@ ctf_arc_write_fd (int fd, ctf_dict_t **ctf_dicts, size_t ctf_dict_cnt,
      memory.)  */
 
   if (ctf_dict_cnt > 0)
-    archdr->ctfa_model = htole64 (ctf_getmodel (ctf_dicts[0]));
+    archdr->ctfa_model = ctf_dict_model (ctf_dicts[0]);
 
   /* Now write out the CTFs: ctf_archive_modent array via the mapping,
      ctfs via write().  The names themselves have not been written yet: we
@@ -807,7 +807,7 @@ ctf_dict_open_by_offset (const struct ctf_archive *arc,
   fp = ctf_bufopen (&ctfsect, symsect, strsect, errp);
   if (fp)
     {
-      ctf_setmodel (fp, le64toh (arc->ctfa_model));
+      ctf_dict_set_model (fp, le64toh (arc->ctfa_model));
       if (little_endian >= 0)
 	ctf_symsect_endianness (fp, little_endian);
     }